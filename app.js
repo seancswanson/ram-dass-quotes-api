c4b1b94c2703c8ecb691470d5a9ca07677b9eac6
seanswanson <sean.swanson@dreambox.com>
2021-03-25 10:47:34 -0700

    Added routes to return all quotes or quotes of a specific category.

--- app.js
+++ app.js
@@ -2,8 +2,42 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+const allQuotes = require('./data/all-quotes.json');
+
+app.get('/api/all', (req, res) => {
+  res.send(allQuotes.payload);
+});
+
+app.get('/api/love', (req, res) => {
+  const loveQuotes = allQuotes.payload.filter(
+    (quote) => quote.category === 'love'
+  );
+  console.log(loveQuotes);
+  res.send(loveQuotes);
+});
+
+app.get('/api/inspiration', (req, res) => {
+  const inspirationQuotes = (allQuotes.payload).filter((quote) => quote.category === 'inspiration');
+  console.log(inspirationQuotes);
+  res.send(inspirationQuotes);
+});
+
+app.get('/api/relationship', (req, res) => {
+  const relationshipQuotes = (allQuotes.payload).filter((quote) => quote.category === 'relationship');
+  console.log(relationshipQuotes);
+  res.send(relationshipQuotes);
+});
+
+app.get('/api/death', (req, res) => {
+  const deathQuotes = (allQuotes.payload).filter((quote) => quote.category === 'death');
+  console.log(deathQuotes);
+  res.send(deathQuotes);
+});
+
+app.get('/api/other', (req, res) => {
+  const otherQuotes = (allQuotes.payload).filter((quote) => quote.category === 'other');
+  console.log(otherQuotes);
+  res.send(otherQuotes);
 });
 
 app.listen(port, () => {
